28547773fa29520cecf889a8651cc2065579ede5
Ghifari <ghifarikautsar8484@gmail.com>
2024-05-13 23:34:37 +0700

    feat(lib) : add new screen jenissampah.dart

---
+++ lib/jenissampah.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class JenisSampah extends StatelessWidget {
+  const JenisSampah({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.yellow,
+      ),
+      body: const Center(
+        child: Text('Mantap men'),
+      ),
+    );
+  }
+}
