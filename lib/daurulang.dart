3edcbe060a16e3de9d1d464779a8a3b8d08718ad
Ghifari <ghifarikautsar8484@gmail.com>
2024-05-13 23:34:21 +0700

    feat(lib) : add new screen daurulang.dart

---
+++ lib/daurulang.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DaurUlang extends StatelessWidget {
+  const DaurUlang({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.yellow,
+      ),
+      body: const Center(
+        child: Text('Mantap men'),
+      ),
+    );
+  }
+}
