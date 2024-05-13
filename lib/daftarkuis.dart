2b81099e227fc0e931f4e92021990955d0b7f8e0
Shinsofy <mfathirizqi@gmail.com>
2024-05-14 03:02:33 +0700

    docs: update daftar kuis dan kuis

--- lib/daftarkuis.dart
+++ lib/daftarkuis.dart
@@ -1,25 +1,6 @@
 import 'package:edutrashgo_mobile/kuis.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Edutrashgo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-      ),
-      home: const DaftarKuis(), // Gunakan Kuis sebagai layar utama
-    );
-  }
-}
-
 class DaftarKuis extends StatefulWidget {
   const DaftarKuis({Key? key}) : super(key: key);
 
