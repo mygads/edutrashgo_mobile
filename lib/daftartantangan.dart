98e04c5feb2a5835e6a6584f8474e2661a86f8e6
Alfathey <asycarye@gmail.com>
2024-05-15 08:48:10 +0700

    fix(daftartantangan) : fix bug

--- lib/daftartantangan.dart
+++ lib/daftartantangan.dart
@@ -1,25 +1,6 @@
 import 'package:edutrashgo_mobile/detailtantangan.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Edutrashgo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-      ),
-      home: const DaftarTantangan(),
-    );
-  }
-}
-
 class DaftarTantangan extends StatefulWidget {
   const DaftarTantangan({super.key});
 
